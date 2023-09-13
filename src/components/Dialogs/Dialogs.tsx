dfb642c49b2878a6b9fd3255d94cef7f3106210f
vronskyvitaly <94834175+vronskyvitaly@users.noreply.github.com>
2023-09-13 14:22:48 +0300

    feat: add styles for the dialogs page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,32 @@ import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogsPages_wrapper}>
+            <div className={s.dialogs}>
+                <div className={s.dialog}>
+                    Виталий
+                </div>
+                <div className={s.dialog}>
+                    Виталий
+                </div>
+                <div className={s.dialog}>
+                    Виталий
+                </div>
+                <div className={s.dialog}>
+                    Виталий
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi
+                </div>
+                <div className={s.message}>
+                    How are you
+                </div>
+                <div className={s.message}>
+                    Sorry
+                </div>
+            </div>
         </div>
     );
 };
