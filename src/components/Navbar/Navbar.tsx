1ec6a80a2843c69ed8155debf11eb39d40b11e89
vronskyvitaly <94834175+vronskyvitaly@users.noreply.github.com>
2023-09-13 13:22:39 +0300

    feat: added NavLink instead of links, which allows you to navigate without restarting the browser

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import s from 'components/Navbar/Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a className={s.link} href="/profile">Profile</a>
+                <NavLink to ="/profile">Profile</NavLink>
             </div>
             <div className={s.item}>
-                <a className={s.link}  href="/dialogs">Messages</a>
+                <NavLink to ="/dialogs">Messages</NavLink>
             </div>
             <div className={s.item}>
                 <a  className={s.link} href={"#!"}>News</a>
