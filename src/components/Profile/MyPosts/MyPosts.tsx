5c5107767b4c10668c68536c6c2bc8cdcbca3a82
vronskyvitaly <94834175+vronskyvitaly@users.noreply.github.com>
2023-09-27 12:40:29 +0300

    feat: accessed textarea value via React.createRef

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, {RefObject} from 'react';
 import {Post} from "components/Profile/MyPosts/Post/Post";
 import s from "./MyPosts.module.css"
 import {PostsDataType} from "redux/state";
 
 
-
 type MyPostsPropsType = {
-    posts:PostsDataType[]
+    posts: PostsDataType[]
 }
 
-export const MyPosts = (props:MyPostsPropsType) => {
+export const MyPosts = (props: MyPostsPropsType) => {
 
 
-    const postsElements = props.posts.map(p=> <Post message={p.message}/>)
+    const postsElements = props.posts.map(p => <Post message={p.message}/>)
+
+    let newPostElement: RefObject<HTMLTextAreaElement> = React.createRef()
+
+    const addPost = () => {
+        let text = newPostElement.current?.value
+        alert(text)
+    }
 
     return (
         <div className={s.my_Posts_wrapper}>
             <div>
                 <h3>My posts</h3>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
                 <div>
                     New posts
