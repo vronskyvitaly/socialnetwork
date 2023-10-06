c8c64528dcd59b3664eab3623b88958c68f75228
vronskyvitaly <94834175+vronskyvitaly@users.noreply.github.com>
2023-10-06 08:30:43 +0300

    feat: added Redirect for rendering the profile when the application starts

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import {Header} from "components/Header/Header";
 import {Navbar} from "components/Navbar/Navbar";
 import {Profile} from "components/Profile/Profile";
 import {Dialogs} from "components/Dialogs/Dialogs";
-import { Route} from "react-router-dom";
+import {Redirect, Route} from "react-router-dom";
 import {StateType} from "redux/state";
+
 
 
 
@@ -21,8 +22,7 @@ function App(props:AppPropsType) {
                 <Header/>
                 <Navbar/>
                 <div className={"app-wrapper-content"}>
-                    {/*<Route path={"/dialogs"} component={Dialogs}/>
-                    <Route path={"/profile"} component={Profile}/>*/}
+                    <Route exact path="/" render={() => <Redirect to="/profile" />} />
                     <Route
                         path={"/dialogs"}
                         render={()=>
